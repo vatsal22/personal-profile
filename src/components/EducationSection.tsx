8d862e966f674edc1e3b426ccb0a7b8a9775067e
Vatsal Solanki <vatsol06@gmail.com>
2025-03-10 02:04:51 -0700

    link to uw website for capstone

--- src/components/EducationSection.tsx
+++ src/components/EducationSection.tsx
@@ -299,6 +299,45 @@ export const EducationSection = ({ education }: EducationSectionProps) => {
                                         communication infrastructure, and
                                         custom-designed hardware components.
                                     </p>
+                                    <div className="mb-4 relative z-20">
+                                        <a
+                                            href="https://www.eng.uwaterloo.ca/2022-capstone-design/electrical-computer/participants#https://www.eng.uwaterloo.ca/2022-capstone-design/electrical-computer/participants/#block-100099342"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="text-[#5D0096] hover:text-[#7E2EAF] text-sm flex items-center transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-purple-500 rounded-md px-1 py-0.5 -ml-1"
+                                            onClick={(e) => {
+                                                e.stopPropagation();
+                                            }}
+                                            onKeyDown={(e) => {
+                                                if (
+                                                    e.key === "Enter" ||
+                                                    e.key === " "
+                                                ) {
+                                                    e.stopPropagation();
+                                                }
+                                            }}
+                                        >
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20"
+                                                fill="currentColor"
+                                                className="w-4 h-4 mr-1"
+                                            >
+                                                <path
+                                                    fillRule="evenodd"
+                                                    d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z"
+                                                    clipRule="evenodd"
+                                                />
+                                                <path
+                                                    fillRule="evenodd"
+                                                    d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z"
+                                                    clipRule="evenodd"
+                                                />
+                                            </svg>
+                                            View on UWaterloo Engineering
+                                            Capstone Design
+                                        </a>
+                                    </div>
 
                                     {/* Image Gallery/Carousel for EyeFly */}
                                     <div className="mt-4 relative">
